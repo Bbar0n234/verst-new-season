e1850b58e4c3d35fe5a40d1291896099b96e31df
Bbar0n234 <vilnus1337@mail.ru>
2023-04-17 13:24:36 +0300

    after lesson 7 full

--- script/main.js
+++ script/main.js
@@ -7,4 +7,16 @@
 			header[0].classList.remove("header_active")
 		}
 	}
-}() )
+}() );
+
+(function () {
+	const burgerMenu = document.getElementsByClassName('burger');
+	const menu = document.getElementsByClassName('header__menu');
+	const burgerMenuCloseEl = document.getElementsByClassName('header__nav-close');
+	burgerMenu[0].addEventListener('click', () => {
+		menu[0].classList.add('header__menu_active');
+	})
+	burgerMenuCloseEl[0].addEventListener('click', () => {
+		menu[0].classList.remove('header__menu_active');
+	})
+} ())
